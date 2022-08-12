1ad41bfe2e167ef1a991d09ce313ec24aac8d68b
Okoli Ekene <khennyphresh@gmail.com>
2022-08-12 20:12:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const countdown = () => {
-    const countDate = new Date('July 29, 2022 00:00:00').getTime();
+    const countDate = new Date('August 29, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
+    const seconds = 1000;
+    const minute = seconds * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
@@ -13,13 +13,13 @@ const countdown = () => {
     const textDay = Math.floor(gap / day);
     const textHour = Math.floor((gap % day) / hour);
     const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute / second));
+    const textSeconds = Math.floor((gap % minute / seconds));
     
     
     document.querySelector(".day").innerText = textDay
     document.querySelector(".hour").innerText = textHour
     document.querySelector(".minute").innerText = textMinute
-    document.querySelector(".second").innerText = textSecond
+    document.querySelector(".seconds").innerText = textSeconds
 }
 
 setInterval(countdown);
